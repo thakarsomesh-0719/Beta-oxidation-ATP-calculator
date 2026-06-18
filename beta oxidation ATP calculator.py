41cf388e5e3cc9ac9872063226939d0b61261e61
Somesh Thakar <thakarsomesh@gmail.com>
2026-06-18 15:15:32 +0530

    Update beta oxidation ATP calculator.py

--- beta oxidation ATP calculator.py
+++ beta oxidation ATP calculator.py
@@ -12,27 +12,27 @@ def ATP_calculator():
         unsat = input('''Please enter 1 if any unsaturation present
 Please enter 0 if no unsaturation present = ''')
     # ---------- calculating total ATP produced on the basis of even/odd chain saturated/unsaturated fatty acids ----------
-        if org_carbon % 2 == 0 and unsat == "0" :
+        if org_carbon % 2 == 0 and unsat == "0" : # For even chain saturated Fatty acids
             total_ATP = (12 * (org_carbon / 2)) + (5 * ((org_carbon / 2) - 1)) - 2
             print(f"The number of ATP genarated by a fatty acid chain having {org_carbon} carbons is {int(total_ATP)} ATP")
             print("Thank You")
             break
-        elif org_carbon % 2 == 0 and unsat == "1":
+        elif org_carbon % 2 == 0 and unsat == "1": # For even chain unsaturated fatty acids
             no_unsat = int(input("Please enter the number of unsaturated bonds "))
             total_ATP = (12 * (org_carbon / 2)) + (5 * ((org_carbon / 2) - 1)) - 2 - (2 * no_unsat)
             print(f"The number of ATP genarated by a fatty acid chain having {org_carbon} carbons is {int(total_ATP)} ATP ")
             print("Thank You")
             break
-        elif org_carbon % 2 != 0 and unsat == "0" :
+        elif org_carbon % 2 != 0 and unsat == "0" : # For odd chain saturated fatty acids
             carbon = org_carbon - 3
-            total_ATP = (12 * (carbon / 2)) + (5 * ((carbon / 2) - 1)) - 3
+            total_ATP = (12 * ((org_carbon/2) - 3)) +  (5 * ((org_carbon/2) - 3)) + 6
             print(f"The number of ATP genarated by a fatty acid chain having {org_carbon} carbons is {int(total_ATP)} ATP ")
             print("Thank You")
             break
         elif org_carbon % 2 != 0 and unsat == "1":
             carbon = org_carbon - 3
             no_unsat = int(input("Please enter the number of unsaturated bonds "))
-            total_ATP = (12 * (carbon / 2)) + (5 * ((carbon / 2) - 1)) - 3 - (2 * no_unsat)
+            total_ATP =  (12 * ((org_carbon/2) - 3)) +  (5 * ((org_carbon/2) - 3)) + 6 - (2 * no_unsat)
             print(f"The number of ATP genarated by a fatty acid chain having {org_carbon} carbons is {int(total_ATP)} ATP")
             print("Thank You")
             break
@@ -41,3 +41,4 @@ Please enter 0 if no unsaturation present = ''')
 
 
 ATP_calculator()
+
